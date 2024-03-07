eb6fbf337b6a3d8fdcd0b28cd1a9fc7d3da7ccd2
khyperia <953151+khyperia@users.noreply.github.com>
2024-03-07 10:17:11 +0100

    initial commit

---
+++ Json.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization.Json;
+
+namespace WarriorsOfWipeBot;
+
+internal class Json<T> where T : new()
+{
+    private static readonly DataContractJsonSerializer Serializer = new(typeof(T),
+        new DataContractJsonSerializerSettings { UseSimpleDictionaryFormat = true });
+
+    private readonly string _jsonFile;
+    public T Data { get; }
+
+    public Json(string jsonFile)
+    {
+        try
+        {
+            using var stream = File.OpenRead(jsonFile);
+            Data = (T)(Serializer.ReadObject(stream) ?? throw new($"Deserialization of {jsonFile} failed"));
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine(jsonFile + " not found, defaulting to empty dict");
+            Data = new();
+        }
+
+        _jsonFile = jsonFile;
+    }
+
+    public void Save()
+    {
+        using var stream = File.Create(_jsonFile);
+        Serializer.WriteObject(stream, Data);
+    }
+}
